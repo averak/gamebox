4a898feb6f3d54416ff5e560900e7b94a9a23ed9
averak <averak.jp@gmail.com>
2024-09-30 02:49:50 +0900

    refs #25 セッションチェックする

--- app/domain/model/user.go
+++ app/domain/model/user.go
@@ -1,7 +1,36 @@
 package model
 
-import "github.com/google/uuid"
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
+
+var (
+	ErrUserDeactivated = errors.New("user is deactivated")
+)
+
+type UserStatus int
+
+const (
+	// 設定が未完了で、他ユーザには公開されていない状態。
+	// サインイン後、設定を完了するまでこの状態が続く。
+	UserStatusPending UserStatus = 1 + iota
+	// アカウントが利用可能で、他ユーザにも公開されている状態。
+	UserStatusActive
+	// アカウントが削除され、利用不可な状態。
+	// この状態になると、二度と利用できなくなる。
+	UserStatusDeactivated
+)
 
 type User struct {
-	ID uuid.UUID
+	ID     uuid.UUID
+	Status UserStatus
+}
+
+func NewUser(id uuid.UUID, status UserStatus) User {
+	return User{
+		ID:     id,
+		Status: status,
+	}
 }
